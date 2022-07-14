fe7e705d7e00a46d78745582f2eab3d3cb999de4
Backley93 <branden.ackley@gmail.com>
2022-07-13 17:51:51 -0700

    Finished creating the basic etch-a-sketch

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,36 @@
+//Reference to the etch-a-sketch container
+
 const grid = document.querySelector('.gridCont');
 
-for (i = 0; i <= 16; i++) {
+//Function for creating grid
+
+let gridSize = null;
+
+let gridArea = null;
+
+let itemSize = null;
+
+const changeGrid = document.querySelector('.change');
+
+changeGrid.addEventListener('click', (e) => {
+    gridSize = prompt('Input a number.', '4');
+    gridSize = parseInt(gridSize);
+    if (gridSize > 100) {
+        alert('Grid can\'t be larger than 100 x 100.');
+        return;
+    }
+    gridArea = gridSize * gridSize;
+    itemSize = (1/gridSize) * 100 + '%';
+    for (i = 1; i <= gridArea; i++) {
     const gridItem = document.createElement('div');
-    gridItem.classList.add('gridItem');
+    gridItem.style.width = itemSize;
+    gridItem.style.height = itemSize;
     grid.appendChild(gridItem);
-}
+}});
+
+
+
+
+
+
+// gridItem.classList.add('gridItem');
