0cb167248b495bee1f7184404ba00f6565b410d3
Backley93 <branden.ackley@gmail.com>
2022-07-16 19:37:14 -0700

    Forgot what I did

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,8 @@
 //Reference to the etch-a-sketch container
 
-const grid = document.querySelector('.gridCont');
+const grid = document.querySelector('.gridcont');
+
+let gridItem = null;
 
 //Function for creating grid
 
@@ -10,27 +12,28 @@ let gridArea = null;
 
 let itemSize = null;
 
-const changeGrid = document.querySelector('.change');
+const changeGrid = document.querySelector('.creategrid');
 
 changeGrid.addEventListener('click', (e) => {
     gridSize = prompt('Input a number.', '4');
     gridSize = parseInt(gridSize);
-    if (gridSize > 100) {
+    if (gridSize > 64) {
         alert('Grid can\'t be larger than 100 x 100.');
         return;
     }
     gridArea = gridSize * gridSize;
     itemSize = (1/gridSize) * 100 + '%';
     for (i = 1; i <= gridArea; i++) {
-    const gridItem = document.createElement('div');
+    gridItem = document.createElement('div');
     gridItem.style.width = itemSize;
     gridItem.style.height = itemSize;
+    gridItem.style.backgroundColor = 'white';
+    gridItem.classList.add('griditem');
     grid.appendChild(gridItem);
+    gridItem.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'blue';
+    })
 }});
 
 
 
-
-
-
-// gridItem.classList.add('gridItem');
